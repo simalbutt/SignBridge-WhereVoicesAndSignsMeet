ebab764505628164f9aba3dd0d87d7031e1161d9
simalbutt <simalbutt04@gamil.com>
2026-01-08 18:02:34 +0500

    integrated tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect } from "react";
-import { testBackend } from "./api/test";
+import React from "react";
+// import { testBackend } from "./api/test";
 
 function App() {
-  useEffect(() => {
-    testBackend();
-  }, []);
-
-  return <h1>SignBridge Frontend Connected</h1>;
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-red-500 text-4xl font-bold">
+        Tailwind Works!
+      </h1>
+    </div>
+  );
 }
 
 export default App;
+
