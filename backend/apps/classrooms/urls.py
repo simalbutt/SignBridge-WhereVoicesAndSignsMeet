ac409a8aa885dd9ee19ddd7a58a0dba4d37858be
simalbutt <simalbutt04@gamil.com>
2026-01-14 00:38:15 +0500

    refacter the delete logic

--- backend/apps/classrooms/urls.py
+++ backend/apps/classrooms/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views.classroom import ClassroomListCreateView, ClassroomRetrieveView, ClassroomDeleteView
+from .views.classroom import (
+    ClassroomListCreateView,
+    ClassroomRetrieveDeleteView,
+)
 from .views.student import ClassroomStudentsView, AddStudentView, RemoveStudentView
 
 urlpatterns = [
-    path("classes/", ClassroomListCreateView.as_view(), name="classroom-list-create"),
-    path("classes/<int:pk>/", ClassroomRetrieveView.as_view(), name="classroom-retrieve"),
-    path("classes/<int:pk>/delete/", ClassroomDeleteView.as_view(), name="classroom-delete"),
+     path("classes/", ClassroomListCreateView.as_view(), name="classroom-list-create"),
+    path("classes/<int:pk>/", ClassroomRetrieveDeleteView.as_view(), name="classroom-retrieve-delete"),
 
     path("<int:classroom_id>/students/", ClassroomStudentsView.as_view()),
     path("<int:classroom_id>/students/add/", AddStudentView.as_view()),
