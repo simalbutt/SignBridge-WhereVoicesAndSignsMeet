d2854e2966d990ba6cba81595960b7c49b2d80f3
Fatima Javed <fatimajaved444@gmail.com>
2026-01-14 21:59:19 +0500

    new feature

--- frontend/src/components/StudentClassCard.jsx
+++ frontend/src/components/StudentClassCard.jsx
@@ -11,7 +11,7 @@ const StudentClassCard = ({ cls }) => {
         onClick={() => navigate(`/student/class/${cls.id}`)}
         className="cursor-pointer rounded-xl bg-gradient-to-br from-teal-50 to-cyan-50 shadow-md hover:shadow-xl transition-all duration-300 overflow-hidden"
       >
-        {/* HEADER */}
+    
         <div className="h-28 bg-gradient-to-r from-teal-400 to-cyan-500 px-4 py-4">
           <h2 className="text-white text-lg font-semibold leading-tight">
             {cls.title}
@@ -19,7 +19,6 @@ const StudentClassCard = ({ cls }) => {
           <p className="text-white text-sm opacity-90">{cls.code}</p>
         </div>
 
-        {/* BODY */}
         <div className="m-4 bg-cyan-100/40 rounded-xl p-4 h-44 flex flex-col justify-between shadow-sm">
           <div className="flex items-center gap-2 text-gray-600">
             <BookOpen className="w-4 h-4 text-teal-600" />
